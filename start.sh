65154c8049092857a08898162a8f50ff49cfb64d
winkxx <68945988+winkxx@users.noreply.github.com>
2022-03-30 19:32:08 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 #!！
 #pip show bgmi
 #bgmi cal --download-cover
-rm -f /etc/filebrowser/database.db
 touch /root/.aria2/aria2.session
 chmod 0777 /root/.aria2/ -R
 #-aria "http://127.0.0.1:8080/jsonrpc"
-nohup filebrowser -r /  -p 9184 >> /dev/null 2>&1 & 
+#nohup filebrowser -r /  -p 9184 >> /dev/null 2>&1 & 
+nohup filebrowser -r / &
 #nohup ./FolderMagic  -auth admin:$Aria2_secret -bind :9184  >> /dev/null 2>&1 & 
 
 mkdir /.config/
